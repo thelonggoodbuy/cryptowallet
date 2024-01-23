12406ddf480f10210cc796bc22c3f52853f3da3b
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2024-01-23 22:18:10 +0200

    login and JWT in work

--- src/users/routers/users_routers.py
+++ src/users/routers/users_routers.py
@@ -18,3 +18,10 @@ async def login():
 
 
 
+
+
+@router.get("/users/profile/", response_class=HTMLResponse)
+async def profile():
+    with open('front/user_profile.html', 'r') as file:
+        data = file.read()
+    return HTMLResponse(content=data, status_code=200)
