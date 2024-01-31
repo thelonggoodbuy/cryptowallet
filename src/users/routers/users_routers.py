290e3daf005486986174ac1a9a4bfa505d3786dd
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2024-01-31 16:32:36 +0200

    redirecting with dependencies work

--- src/users/routers/users_routers.py
+++ src/users/routers/users_routers.py
@@ -1,24 +1,27 @@
-from fastapi import Depends, APIRouter, status, HTTPException, Request
+from fastapi import Cookie, Depends, APIRouter, status, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse, JSONResponse
 
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from src.users.utils import OAuth2PasswordBearerWithCookie
 
 from starlette.responses import RedirectResponse
 
 from datetime import datetime, timedelta, timezone
 
-from typing import Annotated
+from typing import Annotated, Optional
 
 from jose import JWTError, jwt
 from pydantic import BaseModel
 
 from passlib.context import CryptContext
 
+from jose.exceptions import ExpiredSignatureError
+
 
 
 SECRET_KEY = "e902bbf3a6c28106f91028b01e6158bcab2360acc0676243d70404fe6e731b58"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 0.1
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
 
 
 
@@ -30,7 +33,7 @@ fake_users_db = {
         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
     },
-    "alice": {
+    "alice@example.com": {
         "username": "alice",
         "email": "alice@example.com",
         "hashed_password": "fakehashedsecret2",
@@ -72,7 +75,7 @@ class UserInDB(User):
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="token")
 
 
 def verify_password(plain_password, hashed_password):
@@ -113,66 +116,73 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 
 
-# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except JWTError:
-#         raise credentials_exception
-#     user = get_user(fake_users_db, username=token_data.username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
-
-
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(request:Request):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
+        # token in request----
+        token = request.cookies['access_token']
+
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
+
+        user = get_user(fake_users_db, email=token_data.username)
+        if user is None:
+            raise credentials_exception
+        return user
+    
+
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
+    
+    except KeyError:
+        redirect_url = '/users/login/'
+        return RedirectResponse(url=redirect_url, status_code=status.HTTP_307_TEMPORARY_REDIRECT)
+    
 
 
 
+@router.post("/users/validate_access_token/")
+async def validate_access_token(request: Request):
 
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+    json_data = await request.json()
+    token = json_data['access_token']
+    token_from_backend = request.cookies['access_token_in_backend'].replace("Bearer ","")
+
+    if token_from_backend == token:
+
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            result = {'result': True}
+        except ExpiredSignatureError:
+            result = {'result': False, 'cause': 'token_expiced'}
+    else:
+        result = {'result': False, 'cause': 'token_doesnt_match'}
+
+    return result
+
+    # token_from_backend = request.cookies
+    # print('--------SAVED---TOKEN-----')
+    # token_from_backend['access_token_in_backend']
+    # print('-------------------------')
 
 
+    # return result
 
-# OAuth2PasswordRequestForm
+    
+
+
 
 @router.post("/token")
 async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-    # payload: Dict[Any, Any]
-) -> Token:
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], response: Response):
+# ) -> Token:
 
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
 
@@ -184,105 +194,37 @@ async def login_for_access_token(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user.email}, expires_delta=access_token_expires
     )
 
+
+    response.set_cookie(key="access_token_in_backend",value=f"Bearer {access_token}", httponly=True)
     return Token(access_token=access_token, token_type="bearer")
 
 
 
 
 
-
-
-@router.get("/users/me/", response_model=User)
-async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return current_user
-
-
-
-
-@router.get("/users/me/items/")
-async def read_own_items(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
-
-
-
-# def fake_decode_token(token):
-#     return User(
-#         username=token + "fakedecoded", email="john@example.com"
-#     )
-
-
-# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-#     user = fake_decode_token(token)
-#     return user
-
-
-
-
-# @router.get("/users/me")
-# async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
-#     return current_user
-
-
-
-
-
-# @router.get("/users/items/")
-# async def read_items(token: Annotated[str, Depends(get_current_user)]):
-#     return 
-
-
-
-
-
-
-# --------------------------######----------------------------------
-from jose import JWTError, jwt
-
-# class AccessToken(BaseModel):
-#     access_token: str
-
-from fastapi import Request
-
-@router.post("/users/validate_access_token/")
-async def validate_access_token(request: Request):
-    print(request)
-    print(await request.json())
-    json_data = await request.json()
-    token = json_data['access_token']
-    # print('==============')
-    # print(token)
-    # print('==============')
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    print('-------------------JVT-DECODE--------------------------------')
-    print(token)
-    print(payload)
-    print('-------------------!!!!--------------------------------')
-
-
-
-
-
 @router.get("/users/test_response/")
-async def test_response(current_user: Annotated[User, Depends(get_current_active_user)]):
+# async def test_response(current_user: Annotated[User, Depends(get_current_active_user)], request: Request):
+async def test_response(request: Request):
+    # print('#1#')
+    print('---login---cookie---')
+    print(request.cookies)
+    print('--------------------')
     item = {"response": "you are authentoicated!"}
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=item)
 
 
 
 @router.get("/users/login/", response_class=HTMLResponse)
-async def login():
+async def login(request: Request):
 
     with open('front/login.html', 'r') as file:
         data = file.read()
-    
+    print('---login---cookie---')
+    print(request.cookies)
+    print('--------------------')
     return HTMLResponse(content=data, status_code=200)
 
 
@@ -300,7 +242,22 @@ async def send_login_data():
 
 
 @router.get("/users/profile/", response_class=HTMLResponse)
-async def profile(current_user: Annotated[User, Depends(get_current_active_user)]):
-    with open('front/user_profile.html', 'r') as file:
-        data = file.read()
-    return HTMLResponse(content=data, status_code=200)
+async def profile(current_user_or_redirect: Annotated[User, Depends(get_current_user)], 
+                  request: Request,
+                  response: Response,
+                  cookies: Optional[str] = Cookie(None)):
+    
+    # async def profile(request: Request):
+    # print('----profile----cookies---from---Frontend----------------')
+    # print(request.cookies)
+    # print('-----dependency result----------------')
+    # print(current_user_or_redirect)
+    # print(type(current_user_or_redirect))
+
+    match current_user_or_redirect:
+        case UserInDB():
+            with open('front/user_profile.html', 'r') as file:
+                data = file.read()
+            return HTMLResponse(content=data, status_code=200)
+        case RedirectResponse():
+            return current_user_or_redirect
